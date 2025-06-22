ea4bcfc78fedcfc65c1b692a7cd62684cc85d4c5
sja47 <serge.arahal@gmail.com>
2025-06-22 11:27:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -102,15 +102,15 @@ with col3:
 
 with col4:
     st.markdown("**4. Age × Gender Injury/Death Rates**")
-    fig4, ax4 = plt.subplots(figsize=(8.5, 8.0), dpi=120)
+    fig4, ax4 = plt.subplots(figsize=(9.5, 9.0), dpi=120)
     age_gender_avg.plot(kind="bar", ax=ax4, width=0.6, color=["#E45756", "#F28E2B", "#4C78A8", "#76B7B2"])
-    ax4.set_ylabel("Rate per 100k", fontsize=15)
-    ax4.set_xlabel("Age Group", fontsize=15)
-    ax4.tick_params(axis='x', labelsize=15, rotation=30)
-    ax4.tick_params(axis='y', labelsize=15)
+    ax4.set_ylabel("Rate per 100k", fontsize=19)
+    ax4.set_xlabel("Age Group", fontsize=19)
+    ax4.tick_params(axis='x', labelsize=19, rotation=30)
+    ax4.tick_params(axis='y', labelsize=19)
     handles, labels = ax4.get_legend_handles_labels()
     simplified_labels = [label.replace("Death_Rate_per_100k", "Death").replace("Injury_Rate_per_100k", "Injury") for label in labels]
-    ax4.legend(handles, simplified_labels, fontsize=15, loc="center left", bbox_to_anchor=(1, 0.5))
+    ax4.legend(handles, simplified_labels, fontsize=19, loc="center left", bbox_to_anchor=(1, 0.5))
     fig4.tight_layout(pad=0.8)
     st.pyplot(fig4)
 
